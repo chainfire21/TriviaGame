2865336be6d80ab59375383c31522252486a47cd
unknown <paredfed@gmail.com>
2018-09-16 12:05:02 -0500

    added timer bar, and hint at 10 seconds

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,25 +2,25 @@ $(document).ready(function () {
 
     const trivia = {
         questions: [{
-            name: "If I am moving at 0.5c where c is the speed of light, how fast do i measure the speed of light?", q1: "c", q2: "0.5c", q3: "0.25c", q4: "Cannot be calculated", ans: "a", gifSearch: "lightspeed",
+            name: "If I am moving at 0.5c where c is the speed of light, how fast do i measure the speed of light?", q1: "c", q2: "0.5c", q3: "0.25c", q4: "Cannot be calculated", ans: "a", gifSearch: "lightspeed", hint: "b"
         }, {
-            name: "What two things does Heisenbergs Uncertainty Pricnciple say cannot both be measured exactly, at the same time?", q1: "Weight and speed", q2: "Acceleration and mass", q3: "Position and velocity", q4: "Spin and intensity", ans: "c", gifSearch: "uncertainty",
+            name: "What two things does Heisenbergs Uncertainty Pricnciple say cannot both be measured exactly, at the same time?", q1: "Weight and speed", q2: "Acceleration and mass", q3: "Position and velocity", q4: "Spin and intensity", ans: "c", gifSearch: "uncertainty", hint: "b"
         }, {
-            name: "What can escape a black hole's gravitational pull?", q1: "Sound", q2: "Nothing", q3: "Light", q4: "Particles", ans: "b", gifSearch: "black+hole",
+            name: "What can escape a black hole's gravitational pull?", q1: "Sound", q2: "Nothing", q3: "Light", q4: "Particles", ans: "b", gifSearch: "black+hole", hint: "d"
         }, {
-            name: "Which one of these is not a quark name?", q1: "Up", q2: "Charm", q3: "Strange", q4: "Common", ans: "d", gifSearch: "quark+particle",
+            name: "Which one of these is not a quark name?", q1: "Up", q2: "Charm", q3: "Strange", q4: "Common", ans: "d", gifSearch: "quark+particle", hint: "a"
         }, {
-            name: "How much resistance (in Ohms) does a (dry) human have?", q1: "10 ohms", q2: "4,000 ohms", q3: "50,000 ohms", q4: "100,000 ohms", ans: "d", gifSearch: "ohm+resistance",
+            name: "How much resistance (in Ohms) does a (dry) human have?", q1: "10 ohms", q2: "4,000 ohms", q3: "50,000 ohms", q4: "100,000 ohms", ans: "d", gifSearch: "ohm+resistance", hint: "a"
         }, {
-            name: "What element are we made of primarily?", q1: "Sulfur", q2: "Carbon", q3: "Oxygen", q4: "Hydrogen", ans: "c", gifSearch: "oxygen+element",
+            name: "What element are we made of primarily?", q1: "Sulfur", q2: "Carbon", q3: "Oxygen", q4: "Hydrogen", ans: "c", gifSearch: "oxygen+element", hint: "b"
         }, {
-            name: "How many colors are there in the spectrum when white light is separated?", q1: "7", q2: "9", q3: "5", q4: "8", ans: "a", gifSearch: "prism+rainbow",
+            name: "How many colors are there in the spectrum when white light is separated?", q1: "7", q2: "9", q3: "5", q4: "8", ans: "a", gifSearch: "prism+rainbow", hint: "d"
         }, {
-            name: "At what temperature does Fahrenheit equal Celcius?", q1: "42", q2: "100", q3: "-40", q4: "0", ans: "c", gifSearch: "thermometer",
+            name: "At what temperature does Fahrenheit equal Celcius?", q1: "42", q2: "100", q3: "-40", q4: "0", ans: "c", gifSearch: "thermometer", hint: "a"
         }, {
-            name: "What has the highest frequency?", q1: "Gamma rays", q2: "Ultraviolet", q3: "Infrared", q4: "Radio Waves", ans: "a", gifSearch: "gamma+rays",
+            name: "What has the highest frequency?", q1: "Gamma rays", q2: "Ultraviolet", q3: "Infrared", q4: "Radio Waves", ans: "a", gifSearch: "gamma+rays", hint: "b"
         }, {
-            name: "If your twin left on a rocket at birth near the speed of light then came back 10 years later, has their age changed in relation to yours?", q1: "Yes, older", q2: "No", q3: "Yes, younger", q4: "Cannot be known", ans: "c", gifSearch: "twins",
+            name: "If your twin left on a rocket at birth near the speed of light then came back 10 years later, has their age changed in relation to yours?", q1: "Yes, older", q2: "No", q3: "Yes, younger", q4: "Cannot be known", ans: "c", gifSearch: "twins", hint: "b"
         }],
         timeThirty: 30,
         qInterval: 0,
@@ -35,18 +35,17 @@ $(document).ready(function () {
             $("#startGame").on("click", function (e) {
                 $("#questionDiv").removeClass("d-none");
                 trivQ();
-                $("#startGame").addClass("d-none");
+                $("#startInfo").addClass("d-none");
             });
             $(".nextQuestion").on("click", function (e) {
+                $(`#${trivia.questions[trivia.questionNum].hint}`).removeClass("disabled");
                 if ($(this).attr("id") === trivia.questions[trivia.questionNum].ans) {
                     trivia.right++;
                     trivG("y");
-                    console.log(`The amount correct is ${trivia.right}`);
                 }
                 else {
                     trivia.wrong++;
                     trivG("n");
-                    console.log(`The amount incorrect is ${trivia.wrong}`);
                 }
                 trivia.questionNum++;
                 setTimeout(trivQ, 5000);
@@ -63,8 +62,6 @@ $(document).ready(function () {
                 const gO = this.gameOver.bind(this);
                 clearInterval(this.qInterval);
                 gO();
-                console.log("end of questions");
-                console.log(`Final scores are: correct - ${this.right} incorrect - ${this.wrong} unanswered - ${this.unaswered}`)
             }
             else {
                 $("#timeBar").attr("style", `width: 100%`).removeClass("bg-danger").addClass("bg-success");
@@ -83,10 +80,12 @@ $(document).ready(function () {
         thirtyCountDisplay: function () {
             this.timeThirty--;
             const percent = this.timeThirty / 30 * 100;
-            console.log(percent);
             $("#timeBar").attr("style", `width: ${percent}%`);
-            if (this.timeThirty < 16) {
-                $("timeBar").removeClass("bg-success").addClass("bg-danger");
+            if (this.timeThirty === 15) {
+                $("#timeBar").removeClass("bg-success").addClass("bg-danger");
+            }
+            if (this.timeThirty === 10) {
+                this.hint();
             }
             this.checkTimeout();
         },
@@ -94,17 +93,17 @@ $(document).ready(function () {
             if (this.timeThirty === 0) {
                 clearInterval(this.qInterval);
                 this.unaswered++;
-                this.questionNum++;
                 this.question();
                 this.gifImage("t");
                 let trivQ = this.question.bind(this);
+                clearInterval(this.qInterval);
                 setTimeout(trivQ, 5000);
+                $(`#${this.questions[this.questionNum].hint}`).removeClass("disabled");
+                this.questionNum++;
             }
         },
         gifImage: function (check) {
             $("#questionDiv").addClass("d-none");
-            console.log(this);
-            console.log(this.questions[this.questionNum]);
             switch (check) {
                 case "y":
                     $("#response").text(`Correct!`);
@@ -122,7 +121,6 @@ $(document).ready(function () {
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
-                console.log(response);
                 // //this puts the image url from the api into the image tag src
                 $("#gif").html($("<img>").attr("src", response.data[0].images.fixed_height.url));
                 return null
@@ -145,6 +143,10 @@ $(document).ready(function () {
             this.unaswered = 0;
             this.wrong = 0;
             this.qInterval = setInterval(this.thirtyCountDisplay.bind(this), 1000);
+        },
+        hint: function () {
+            $(`#${this.questions[this.questionNum].hint}`).addClass("disabled");
+
         }
 
     }
